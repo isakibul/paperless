749dd85916b5adee8b1263cef1be79cc64495148
isakibul <isakibul108@gmail.com>
2025-01-06 13:32:59 +0600

    static keyword

---
+++ StaticKeyword.java
@@ -0,0 +1,12 @@
+public class StaticKeyword {
+    static int age = 23;
+
+    static void display(){
+        System.out.println("lorem ipsum");
+    }
+
+    public static void main(String args[]){
+        System.out.println(age);
+        display();
+    }
+}
